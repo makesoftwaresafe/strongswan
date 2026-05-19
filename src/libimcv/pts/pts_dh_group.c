makesoftwaresafe/strongswan

commit 0841280cdde884617f5ef40470057633a44d3a1f

libimcv: Fix build with DEBUG_LEVEL < 3

src/libimcv/pts/pts_dh_group.c
@@ -26,43 +26,40 @@ bool pts_dh_group_probe(pts_dh_group_t *dh_groups, bool mandatory_dh_groups)
 	enumerator_t *enumerator;
 	key_exchange_method_t dh_group;
 	const char *plugin_name;
-	char format1[] = "  %s PTS DH group %N[%s] available";
-	char format2[] = "  %s PTS DH group %N not available";
 
 	*dh_groups = PTS_DH_GROUP_NONE;
 
 	enumerator = lib->crypto->create_ke_enumerator(lib->crypto);
 	while (enumerator->enumerate(enumerator, &dh_group, &plugin_name))
 	{
-		if (dh_group == MODP_1024_BIT)
+		pts_dh_group_t mapped = PTS_DH_GROUP_NONE;
+
+		switch (dh_group)
 		{
-			*dh_groups |= PTS_DH_GROUP_IKE2;
-			DBG2(DBG_PTS, format1, "optional ", key_exchange_method_names,
-									dh_group, plugin_name);
+			case MODP_1024_BIT:
+				mapped = PTS_DH_GROUP_IKE2;
+				break;
+			case MODP_1536_BIT:
+				mapped = PTS_DH_GROUP_IKE5;
+				break;
+			case MODP_2048_BIT:
+				mapped = PTS_DH_GROUP_IKE14;
+				break;
+			case ECP_256_BIT:
+				mapped = PTS_DH_GROUP_IKE19;
+				break;
+			case ECP_384_BIT:
+				mapped = PTS_DH_GROUP_IKE20;
+				break;
+			default:
+				break;
 		}
-		else if (dh_group == MODP_1536_BIT)
+		if (mapped != PTS_DH_GROUP_NONE)
 		{
-			*dh_groups |= PTS_DH_GROUP_IKE5;
-			DBG2(DBG_PTS, format1, "optional ", key_exchange_method_names,
-									dh_group, plugin_name);
-		}
-		else if (dh_group == MODP_2048_BIT)
-		{
-			*dh_groups |= PTS_DH_GROUP_IKE14;
-			DBG2(DBG_PTS, format1, "optional ", key_exchange_method_names,
-									dh_group, plugin_name);
-		}
-		else if (dh_group == ECP_256_BIT)
-		{
-			*dh_groups |= PTS_DH_GROUP_IKE19;
-			DBG2(DBG_PTS, format1, "mandatory", key_exchange_method_names,
-									dh_group, plugin_name);
-		}
-		else if (dh_group == ECP_384_BIT)
-		{
-			*dh_groups |= PTS_DH_GROUP_IKE20;
-			DBG2(DBG_PTS, format1, "optional ", key_exchange_method_names,
-									dh_group, plugin_name);
+			*dh_groups |= mapped;
+			DBG2(DBG_PTS, "  %s PTS DH group %N[%s] available",
+				 mapped == PTS_DH_GROUP_IKE19 ? "mandatory" : "optional ",
+				 key_exchange_method_names, dh_group, plugin_name);
 		}
 	}
 	enumerator->destroy(enumerator);
@@ -79,8 +76,8 @@ bool pts_dh_group_probe(pts_dh_group_t *dh_groups, bool mandatory_dh_groups)
 	}
 	if (mandatory_dh_groups)
 	{
-		DBG1(DBG_PTS, format2, "mandatory", key_exchange_method_names,
-											ECP_256_BIT);
+		DBG1(DBG_PTS, "  mandatory PTS DH group %N[%s] available",
+			 key_exchange_method_names, ECP_256_BIT);
 		return FALSE;
 	}
 

src/libimcv/generic/generic_attr_bool.c
@@ -121,7 +121,7 @@ METHOD(pa_tnc_attr_t, build, void,
 METHOD(pa_tnc_attr_t, process, status_t,
 	private_generic_attr_bool_t *this, uint32_t *offset)
 {
-	enum_name_t *pa_attr_names;
+	enum_name_t *pa_attr_names DBG_UNUSED;
 	bio_reader_t *reader;
 	uint32_t status;
 

src/libimcv/generic/generic_attr_chunk.c
@@ -96,7 +96,7 @@ METHOD(pa_tnc_attr_t, build, void,
 METHOD(pa_tnc_attr_t, process, status_t,
 	private_generic_attr_chunk_t *this, uint32_t *offset)
 {
-	enum_name_t *pa_attr_names;
+	enum_name_t *pa_attr_names DBG_UNUSED;
 	*offset = 0;
 
 	if (this->value.len < this->length)

src/libimcv/generic/generic_attr_string.c
@@ -91,7 +91,7 @@ METHOD(pa_tnc_attr_t, build, void,
 METHOD(pa_tnc_attr_t, process, status_t,
 	private_generic_attr_string_t *this, uint32_t *offset)
 {
-	enum_name_t *pa_attr_names;
+	enum_name_t *pa_attr_names DBG_UNUSED;
 	u_char *pos;
 	*offset = 0;
 

src/libimcv/imc/imc_agent.c
@@ -346,7 +346,7 @@ METHOD(imc_agent_t, create_state, TNC_Result,
 {
 	TNC_ConnectionID conn_id;
 	char *tnccs_p = NULL, *tnccs_v = NULL, *t_p = NULL, *t_v = NULL;
-	bool has_long = FALSE, has_excl = FALSE, has_soh = FALSE;
+	bool has_long = FALSE, has_excl = FALSE, has_soh DBG_UNUSED = FALSE;
 	uint32_t max_msg_len;
 
 	conn_id = state->get_connection_id(state);

src/libimcv/imc/imc_msg.c
@@ -254,7 +254,6 @@ METHOD(imc_msg_t, receive, TNC_Result,
 	enumerator_t *enumerator;
 	pa_tnc_attr_t *attr;
 	pen_type_t attr_type;
-	chunk_t msg;
 	bool first = TRUE;
 
 	if (this->state->has_long(this->state))
@@ -282,8 +281,10 @@ METHOD(imc_msg_t, receive, TNC_Result,
 					   this->agent->get_name(this->agent),
 					   this->connection_id);
 	}
-	msg = this->pa_msg->get_encoding(this->pa_msg);
+#if DEBUG_LEVEL >= 3
+	chunk_t msg = this->pa_msg->get_encoding(this->pa_msg);
 	DBG3(DBG_IMC, "%B", &msg);
+#endif
 
 	switch (this->pa_msg->process(this->pa_msg))
 	{
@@ -522,7 +523,7 @@ METHOD(imc_msg_t, receive, TNC_Result,
 		{
 			ietf_attr_remediation_instr_t *attr_cast;
 			pen_type_t parameters_type;
-			chunk_t parameters, string, lang_code;
+			chunk_t parameters DBG_UNUSED, string DBG_UNUSED, lang_code;
 
 			attr_cast = (ietf_attr_remediation_instr_t*)attr;
 			parameters_type = attr_cast->get_parameters_type(attr_cast);

src/libimcv/imv/imv_agent.c
@@ -413,7 +413,7 @@ METHOD(imv_agent_t, create_state, TNC_Result,
 {
 	TNC_ConnectionID conn_id;
 	char *tnccs_p = NULL, *tnccs_v = NULL, *t_p = NULL, *t_v = NULL;
-	bool has_long = FALSE, has_excl = FALSE, has_soh = FALSE;
+	bool has_long = FALSE, has_excl = FALSE, has_soh DBG_UNUSED = FALSE;
 	linked_list_t *ar_identities;
 	imv_session_t *session;
 	uint32_t max_msg_len;

src/libimcv/imv/imv_msg.c
@@ -293,7 +293,6 @@ METHOD(imv_msg_t, receive, TNC_Result,
 	linked_list_t *non_fatal_types;
 	enumerator_t *enumerator;
 	pa_tnc_attr_t *attr;
-	chunk_t msg;
 
 	if (this->state->has_long(this->state))
 	{
@@ -320,8 +319,10 @@ METHOD(imv_msg_t, receive, TNC_Result,
 					   this->agent->get_name(this->agent),
 					   this->connection_id);
 	}
-	msg = this->pa_msg->get_encoding(this->pa_msg);
+#if DEBUG_LEVEL >= 3
+	chunk_t msg = this->pa_msg->get_encoding(this->pa_msg);
 	DBG3(DBG_IMV, "%B", &msg);
+#endif
 
 	switch (this->pa_msg->process(this->pa_msg))
 	{

commit 4950a37e562cf439eb156aee5409506a824d9b9e

pts: Fix error string if mandatory DH group is unavailable
Fixes: 0841280cdde8 ("libimcv: Fix build with DEBUG_LEVEL < 3")

src/libimcv/pts/pts_dh_group.c
@@ -76,7 +76,7 @@ bool pts_dh_group_probe(pts_dh_group_t *dh_groups, bool mandatory_dh_groups)
 	}
 	if (mandatory_dh_groups)
 	{
-		DBG1(DBG_PTS, "  mandatory PTS DH group %N[%s] available",
+		DBG1(DBG_PTS, "  mandatory PTS DH group %N not available",
 			 key_exchange_method_names, ECP_256_BIT);
 		return FALSE;
 	}